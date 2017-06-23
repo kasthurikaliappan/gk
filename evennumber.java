75c28e111136481379e4fe747fae66064f16e407
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-23 14:23:36 +0530

    Add files via upload

---
+++ evennumber.java
@@ -0,0 +1,12 @@
+package gk;
+import java.util.*;
+public class evennumber {
+ public static void main(String args[]){
+	 int i;
+	 for(i=1; i<10; i++){
+		 if(i%2==0){
+			 System.out.println(""+i);
+		 }
+	 }
+ }
+}
