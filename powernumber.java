3f72c2ad6c6a6bba10fd8fd1e19b3547ee4bb21d
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-23 12:36:08 +0530

    Add files via upload

---
+++ powernumber.java
@@ -0,0 +1,18 @@
+package gk;
+import java.util.*;
+public class powernumber {
+	public static void main(String args[])
+	{
+  Scanner sc=new Scanner(System.in);
+  int s=3;
+  int e=5;
+  int res=1;
+  while(e!=0)
+  {
+		 res=res*e;
+		 e--;
+  }
+  System.out.println(res);
+  
+}
+}
